8bc37603829f1d6af9288ecf0b96967a37f47bb3
Vibhuti <vibhuti9926@gmail.com>
2018-07-05 11:08:49 +0530

    project architecture

---
+++ app/src/main/java/datingapp/com/datingapp/BaseView.java
@@ -0,0 +1,12 @@
+package datingapp.com.datingapp;
+
+/**
+ * Base View interface to be extended by all Views of the app
+ */
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+
+
+}
---
+++ app/src/main/java/datingapp/com/datingapp/ui/DatingActivity.java
@@ -0,0 +1,15 @@
+package datingapp.com.datingapp.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import datingapp.com.datingapp.R;
+
+public class DatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dating);
+    }
+}
